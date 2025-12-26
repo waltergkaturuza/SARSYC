4aceaecf6abc78c5eadfa020fd1eb2946e6c5dda
Automation <devnull@example.com>
2025-12-26 15:17:12 +0200

    Add React import to RegistrationActionButtons component

--- src/components/admin/RegistrationActionButtons.tsx
+++ src/components/admin/RegistrationActionButtons.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { FiCheckCircle, FiXCircle, FiLoader } from 'react-icons/fi'
 
