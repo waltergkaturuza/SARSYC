d4d1a2e60f686cb63d72eb0ec336e987fdf64b63
Automation <devnull@example.com>
2025-12-27 11:40:06 +0200

    fix: Add sponsorship tiers migration to migrations index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20251223_130213 from './20251223_130213';
 import * as migration_20251224_120000 from './20251224_120000';
 import * as migration_20251226_155456_add_international_registration_fields from './20251226_155456_add_international_registration_fields';
 import * as migration_20251226_160419_add_passport_scan_nextofkin_enhancements from './20251226_160419_add_passport_scan_nextofkin_enhancements';
+import * as migration_20251227_113630_create_sponsorship_tiers from './20251227_113630_create_sponsorship_tiers';
 
 export const migrations = [
   {
@@ -23,5 +24,10 @@ export const migrations = [
     up: migration_20251226_160419_add_passport_scan_nextofkin_enhancements.up,
     down: migration_20251226_160419_add_passport_scan_nextofkin_enhancements.down,
     name: '20251226_160419_add_passport_scan_nextofkin_enhancements'
+  },
+  {
+    up: migration_20251227_113630_create_sponsorship_tiers.up,
+    down: migration_20251227_113630_create_sponsorship_tiers.down,
+    name: '20251227_113630_create_sponsorship_tiers'
   },
 ];
